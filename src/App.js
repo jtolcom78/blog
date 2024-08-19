991ce5650c1b0592578330bbc6c404e1c8d0c6cf
SeokHyun Jee <jtolcom78@gmail.com>
2024-08-19 16:13:43 +0700

    p5

--- src/App.js
+++ src/App.js
@@ -29,12 +29,12 @@ function App() {
       </div>
       <div className='list'>
         <h4>{글제목[2]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
-        <p>2월 17일 발행p3</p>
+        <p>2월 17일 발행</p>
       </div>
 
 
       <Modal>
-        
+
       </Modal>
 
     </div>
